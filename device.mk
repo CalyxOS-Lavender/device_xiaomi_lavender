be64dee613a8d0fa898ae232af65211c44481b51
dianlujitao <dianlujitao@lineageos.org>
2019-01-29 13:39:57 +0800

    lavender: Build camera HAL from source
    Change-Id: I54e122b3df6308e205d8ee93d55a3adf7469412d
    Signed-off-by: pix106 <sbordenave@gmail.com>

--- device.mk
+++ device.mk
@@ -67,6 +67,7 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    camera.sdm660 \
     Aperture \
     android.hardware.camera.provider@2.4-impl:32 \
     android.hardware.camera.provider@2.4-service
@@ -442,8 +443,8 @@ PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 PRODUCT_SHIPPING_API_LEVEL := 28
 
 # Shims
-PRODUCT_PACKAGES += \
-    libcamera_sdm660_shim
+#PRODUCT_PACKAGES += \
+#    libcamera_sdm660_shim
 
 # Telephony
 PRODUCT_PACKAGES += \
