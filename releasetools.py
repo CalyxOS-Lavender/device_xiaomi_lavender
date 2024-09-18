15755c0ba1b5d1f9429452daebfc6855ea7ee1b7
wHo-EM-i <ehteshammalik18998@gmail.com>
2024-09-18 20:02:16 +0530

    lavender: Always flash super_image
    Instead of checking non-rdp lets just flash super image and do mapping afterwards
    
    Signed-off-by: wHo-EM-i <ehteshammalik18998@gmail.com>
    Signed-off-by: Aeoniixx <aeoniixx@gmail.com>

--- releasetools.py
+++ releasetools.py
@@ -5,33 +5,30 @@
 #
 
 import common
+import re
 
 def FullOTA_InstallEnd(info):
   OTA_InstallEnd(info)
-  return
 
 def IncrementalOTA_InstallEnd(info):
   OTA_InstallEnd(info)
-  return
 
 def AddImage(info, dir, basename, dest):
-  path = dir + "/" + basename
-  if path not in info.input_zip.namelist():
-    return
-
-  data = info.input_zip.read(path)
-  common.ZipWriteStr(info.output_zip, basename, data)
-  info.script.AppendExtra('package_extract_file("%s", "%s");' % (basename, dest))
+  name = basename
+  data = info.input_zip.read(dir + "/" + basename)
+  common.ZipWriteStr(info.output_zip, name, data)
+  info.script.AppendExtra('package_extract_file("%s", "%s");' % (name, dest))
 
 def FullOTA_InstallBegin(info):
-  AddImage(info, "RADIO", "super_dummy.img", "/tmp/super_dummy.img");
-  info.script.AppendExtra('package_extract_file("install/bin/flash_super_dummy.sh", "/tmp/flash_super_dummy.sh");')
-  info.script.AppendExtra('set_metadata("/tmp/flash_super_dummy.sh", "uid", 0, "gid", 0, "mode", 0755);')
-  info.script.AppendExtra('run_program("/tmp/flash_super_dummy.sh");')
+  info.script.AppendExtra('ifelse(is_mounted("/system_root"),unmount("/system_root"));')
+  info.script.AppendExtra('ifelse(is_mounted("/vendor"),unmount("/vendor"));')
   info.script.AppendExtra('assert(getprop("ro.boot.super_partition") == "system" || abort("ERROR: This recovery does not support retrofit dynamic partitions."););')
+  info.script.AppendExtra('run_program("/system/bin/toybox", "blkdiscard", "/dev/block/bootdevice/by-name/system"); || abort("ERROR: Failed to discard data on system partition.");')
+  info.script.AppendExtra('run_program("/system/bin/toybox", "blkdiscard", "/dev/block/bootdevice/by-name/vendor"); || abort("ERROR: Failed to discard data on vendor partition.");')
+  info.script.AppendExtra('ui_print("- Flashing super_empty onto system partition...");')
+  AddImage(info, "RADIO", "super_dummy.img", "/dev/block/bootdevice/by-name/system");
   return
 
 def OTA_InstallEnd(info):
   info.script.Print("Patching device-tree and verity images...")
   AddImage(info, "IMAGES", "vbmeta.img", "/dev/block/bootdevice/by-name/vbmeta")
-  return
