7649671e55b745fc2a71f97f063f48cd61d9560e
webgeek1234 <1706847+webgeek1234@users.noreply.github.com>
2025-03-25 10:53:44 +0700

    lavender: Re-enable broken dup rules check
    All failures of this have been resolved

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -36,9 +36,6 @@ TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a73
 
-# Allow broken rules
-BUILD_BROKEN_DUP_RULES := true
-
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := sdm660
 TARGET_NO_BOOTLOADER := true
